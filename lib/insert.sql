f131e10b3f6594a991bc8f2ee689c5ecdb5dc148
J Vance <justinleevance94@gmail.com>
2024-02-09 10:52:33 -0500

    This helped

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) Values (1, "Mr. Chocolate", 2, "Male", "Chocolate", "Nice", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) Values (2, "Rowdy", 3, "Male", "Tabbey", "Mean", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) Values (3, "Tabitha", 22, "Female", "White", "Old but nice", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) Values (4, "Sergeant Brown", 3, "Male", "Grey", "Nice", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) Values (5, "Melissa", 10, "Female", "Pink", "Nice", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) Values (6, "Grinch", 2, "Male", "Green", "Naughty", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) Values (7, "Wendy", 7, "Female", "Grey", "Lovely", 1);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) Values (8, NULL, 2, "Male", "Chocolate", "Nice", 1);
+
+
+
+
+
+
